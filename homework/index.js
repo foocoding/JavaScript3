40ae65d3f7a8dc4072063e1fd3d2aa01c214dc8d
Rawaa Al Kabbani <rawaa@yahoo.com>
2019-10-11 18:39:57 +0200

    changes to week1-version2 to be week2

--- homework/index.js
+++ homework/index.js
@@ -1,32 +1,17 @@
-const fetchRepositories = async url => {
-  try {
-    const response = await fetch(url);
-    const dataList = await response.json();
-    return dataList;
-  }
-  catch (err) {
-    alert(err)
-  }
-};
-const logData = async (url) => {
-  const repositoriesData = await fetchRepositories(url);
-  const FooCodingRepositories = repositoriesData.sort((a, b) =>
-    a.name.localeCompare(b.name, 'fr', { ignorePunctuation: true }));
-  createPage(FooCodingRepositories);
-};
-const logContributors = async (url) => {
-  const contributorsData = await fetchRepositories(url);
-  const FooCodingContributors = contributorsData.sort((a, b) =>
-    a.login.localeCompare(b.login, 'fr', { ignorePunctuation: true }));
-  handleContributors(FooCodingContributors);
-};
-
+const mainDiv = document.body;
 function createElement(parentElement, element, nameId) {
   const newElement = document.createElement(element);
   newElement.setAttribute('id', nameId);
   parentElement.appendChild(newElement);
   return newElement;
 }
+
+const fetchRepositories = async url => {
+  const response = await fetch(url);
+  const dataList = await response.json();
+  return dataList;
+};
+
 const createOption = (item, index) => {
   const newOption = document.createElement('option');
   const repositoriesSelect = document.getElementById('repositoriesSelect');
@@ -34,13 +19,22 @@ const createOption = (item, index) => {
   newOption.text = item.name;
   newOption.value = index;
   repositoriesSelect.appendChild(newOption);
+};
+
+function appendToLi(indexLi, element, nameIdP1, textNodeP1, nameIdP2, textNodeP2) {
+  const p1 = createElement(indexLi, element, nameIdP1);
+  const contentP1 = document.createTextNode(textNodeP1);
+  p1.appendChild(contentP1);
+  const p2 = createElement(indexLi, element, nameIdP2);
+  const contentP2 = document.createTextNode(textNodeP2);
+  p2.appendChild(contentP2);
 }
 
 function createList(parentElement, element, lio, li1, li2, li3, aUrl) {
   for (let y = 0; y < 4; y++) {
     createElement(parentElement, element, `li${y}`);
   }
-  const liTags = document.getElementsByTagName('li')
+  const liTags = document.getElementsByTagName('li');
   const p0 = createElement(liTags[0], 'p', 'repository');
   const contentP0 = document.createTextNode('Repository:');
   p0.appendChild(contentP0);
@@ -53,14 +47,43 @@ function createList(parentElement, element, lio, li1, li2, li3, aUrl) {
   appendToLi(liTags[3], 'p', 'updated', 'Updated:', 'updatedValue', li3);
 }
 
-function appendToLi(indexLi, element, nameIdP1, textNodeP1, nameIdP2, textNodeP2) {
-  const p1 = createElement(indexLi, element, nameIdP1);
-  const contentP1 = document.createTextNode(textNodeP1);
-  p1.appendChild(contentP1);
-  const p2 = createElement(indexLi, element, nameIdP2);
-  const contentP2 = document.createTextNode(textNodeP2);
-  p2.appendChild(contentP2);
+function handleContributors(contributors) {
+  const rightDiv = document.getElementById('rightDiv');
+  rightDiv.innerHTML = '';
+  const contributorsTitle = createElement(rightDiv, 'div', 'contributorsTitle');
+  const contributorsTitleContent = document.createTextNode('Contributors');
+  contributorsTitle.appendChild(contributorsTitleContent);
+  contributors.forEach(contributor => {
+    // create subDiv for each contributor
+    const subDiv = document.createElement('div');
+    subDiv.className = 'contributor';
+    rightDiv.appendChild(subDiv);
+    const image = document.createElement('img');
+    image.className = 'image';
+    image.setAttribute('src', contributor.avatar_url);
+    subDiv.appendChild(image);
+    const login = createElement(subDiv, 'p', 'login');
+    const loginContent = document.createTextNode(contributor.login);
+    login.appendChild(loginContent);
+    const contributions = createElement(subDiv, 'p', 'contributions');
+    const contributionsContent = document.createTextNode(contributor.contributions);
+    contributions.appendChild(contributionsContent);
+  });
 }
+
+const logContributors = async url => {
+  try {
+    const contributorsData = await fetchRepositories(url);
+    const FooCodingContributors = contributorsData.sort((a, b) =>
+      a.login.localeCompare(b.login, 'fr', { ignorePunctuation: true }),
+    );
+    handleContributors(FooCodingContributors);
+  } catch (error) {
+    const errorDiv = createElement(mainDiv, 'div', 'errorDiv');
+    const errorContent = document.createTextNode('error');
+    errorDiv.appendChild(errorContent);
+  }
+};
 
 function createPage(repositories) {
   // create upperDiv
@@ -76,7 +99,17 @@ function createPage(repositories) {
   const leftDiv = createElement(leftRightDiv, 'div', 'leftDiv');
   const repositoryDetails = createElement(leftDiv, 'ul', 'repositoryDetails');
   const defaultRepository = repositories[0];
-  createList(repositoryDetails, 'li', defaultRepository.name, defaultRepository.description, defaultRepository.forks, defaultRepository.updated_at, defaultRepository.html_url);
+  const updated = defaultRepository.updated_at;
+  const formatUpdated = updated.replace(/T/, ', ').replace(/Z/, '');
+  createList(
+    repositoryDetails,
+    'li',
+    defaultRepository.name,
+    defaultRepository.description,
+    defaultRepository.forks,
+    formatUpdated,
+    defaultRepository.html_url,
+  );
   // create rightDiv
   createElement(leftRightDiv, 'div', 'rightDiv');
   // fetch contributors to implement the rightDiv
@@ -89,31 +122,32 @@ function createPage(repositories) {
       liElement[i].parentElement.removeChild(liElement[i]);
     }
     const selectedRepository = repositories[select.value];
-    createList(repositoryDetails, 'li', 'detailsList', selectedRepository.name, selectedRepository.description, selectedRepository.forks, selectedRepository.updated_at);
+    const updatedForSelect = selectedRepository.updated_at;
+    const formatUpdatedForSelect = updatedForSelect.replace(/T/, ', ').replace(/Z/, '');
+    createList(
+      repositoryDetails,
+      'li',
+      selectedRepository.name,
+      selectedRepository.description,
+      selectedRepository.forks,
+      formatUpdatedForSelect,
+    );
     const ContributorsUrl = selectedRepository.contributors_url;
     logContributors(ContributorsUrl);
   });
 }
 
-function handleContributors(contributors) {
-  const rightDiv = document.getElementById('rightDiv');
-  rightDiv.innerHTML = '';
-  contributors.forEach(contributor => {
-    // create subDiv for each contributor
-    const subDiv = document.createElement('div');
-    subDiv.className = 'contributor'
-    rightDiv.appendChild(subDiv);
-    const image = document.createElement('img');
-    image.className = 'image';
-    image.setAttribute('src', contributor.avatar_url);
-    subDiv.appendChild(image);
-    const login = createElement(subDiv, 'p', 'login');
-    const loginContent = document.createTextNode(contributor.login)
-    login.appendChild(loginContent);
-    const contributions = createElement(subDiv, 'p', 'contributions');
-    const contributionsContent = document.createTextNode(contributor.contributions)
-    contributions.appendChild(contributionsContent);
-  });
-}
-const mainDiv = document.body;
+const logData = async url => {
+  try {
+    const repositoriesData = await fetchRepositories(url);
+    const FooCodingRepositories = repositoriesData.sort((a, b) =>
+      a.name.localeCompare(b.name, 'fr', { ignorePunctuation: true }),
+    );
+    createPage(FooCodingRepositories);
+  } catch (error) {
+    const errorDiv = createElement(mainDiv, 'div', 'errorDiv');
+    const errorContent = document.createTextNode('error');
+    errorDiv.appendChild(errorContent);
+  }
+};
 logData('https://api.github.com/orgs/foocoding/repos?per_page=100');
